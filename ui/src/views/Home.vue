502d21662e8362915d7a9c4ebb12198a7a625dbc
luoyi <1226883918@qq.com>
2024-04-16 16:26:54 +0800

    baseurl

--- ui/src/views/Home.vue
+++ ui/src/views/Home.vue
@@ -260,7 +260,7 @@ export default {
     },
     handleUserCommand(command) {
       if (command === 'logout') {
-        this.axios.get('/signout').then(() => {
+        this.axios.get('/transip-control/signout').then(() => {
           this.$router.push('/login');
         });
       }
@@ -272,7 +272,7 @@ export default {
       });
     },
     getDevices() {
-      this.axios.get('/devs').then(res => {
+      this.axios.get('/transip-control/devs').then(res => {
         this.loading = false;
         this.devlists = res.data;
         this.selection = [];
@@ -296,7 +296,7 @@ export default {
         return;
       }
 
-      this.axios.get('/users').then(res => {
+      this.axios.get('/transip-control/users').then(res => {
         this.bindUserData.users = res.data.users;
         this.bindUserData.modal = true;
       });
@@ -304,7 +304,7 @@ export default {
     bindUser() {
       this.bindUserData.modal = false;
 
-      this.axios.post('/bind', {
+      this.axios.post('/transip-control/bind', {
         devices: this.selection.map(s => s.id),
         username: this.bindUserData.currentUser
       }).then(() => {
@@ -313,7 +313,7 @@ export default {
       });
     },
     unBindUser(id) {
-      this.axios.post('/unbind', {
+      this.axios.post('/transip-control/unbind', {
         devices: [id]
       }).then(() => {
         this.getDevices();
@@ -333,7 +333,7 @@ export default {
         return;
       }
 
-      this.axios.post('/delete', {
+      this.axios.post('/transip-control/delete', {
         devices: offlines.map(s => s.id)
       }).then(() => {
         this.getDevices();
@@ -507,7 +507,7 @@ export default {
   mounted() {
     this.username = sessionStorage.getItem('rttys-username') || '';
 
-    this.axios.get('/isadmin').then(res => {
+    this.axios.get('/transip-control/isadmin').then(res => {
       this.isadmin = res.data.admin;
     });
 
