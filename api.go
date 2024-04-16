34fba4a0604dab72b18f8926db31a91dbb87016c
hlhgogo <hlhgogo@163.com>
2024-04-16 17:08:11 +0800

    修改前缀

--- api.go
+++ api.go
@@ -207,7 +207,7 @@ func apiStart(br *broker) {
 
 	authorized.GET("/connect/:devid", func(c *gin.Context) {
 		if c.GetHeader("Upgrade") != "websocket" {
-			url := "/rtty/" + c.Param("devid")
+			url := ApiPrefix + "/rtty/" + c.Param("devid")
 			if c.Query("jwt") != "" {
 				url = url + "?jwt=" + c.Query("jwt")
 			}
