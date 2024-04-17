01eeb96a3a9c4919ef12b2101370d726293fdede
luoyi <1226883918@qq.com>
2024-04-17 14:19:04 +0800

    baseurl

--- ui/src/views/Rtty.vue
+++ ui/src/views/Rtty.vue
@@ -226,7 +226,7 @@ export default {
   mounted() {
     const protocol = (location.protocol === 'https:') ? 'wss://' : 'ws://';
     this.deviceName = new URLSearchParams(window.location.href.split('?')[1]).get('name');
-    const socket = new WebSocket(protocol + location.host + `/connect/${this.devid}?jwt=${new URLSearchParams(window.location.href.split('?')[1]).get('jwt')}`);
+    const socket = new WebSocket(protocol + location.host + `/transip-control/connect/${this.devid}?jwt=${new URLSearchParams(window.location.href.split('?')[1]).get('jwt')}`);
     socket.binaryType = 'arraybuffer';
     this.socket = socket;
 
